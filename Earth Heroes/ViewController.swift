bfb17286deccbab65ed97cd4abda9beb6fbde7fa
Molly Tenenbaum <mollytenenbaum100@gmail.com>
2020-07-29 08:26:11 -0700

    Initial Commit

---
+++ Earth Heroes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Earth Heroes
+//
+//  Created by Molly Tenenbaum on 7/29/20.
+//  Copyright © 2020 Molly Tenenbaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
